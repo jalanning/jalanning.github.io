69c24dcea3d5012f5a34f134e8e527872d027773
Jake Lanning <lanning.jake.w@gmail.com>
2022-09-30 17:12:59 -0400

    complete!

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ const pageTextAndPic = [
     ["Traveling is in your blood. We will complete this map together, then onto the maps of other countries as well! Maybe we set up a real home base here in the D, maybe we don't, but we will never stop finding new places to explore.","https://i.imgur.com/SW8t3is.png"],
     ["TV is getting fun!! I never thought you'd convince me to enjoy reality TV, but boy does 'Are you the one?` get exciting! I can't wait for the next episode of Handmaids and to finish `Better Call Saul'. And let's not forget I owe you a date to the cinema!","https://64.media.tumblr.com/0c29e6c8170aa87f5fc0242fc7648360/tumblr_mq2f2w805G1rtd3oeo1_500.gifv"],
     ["You are just so adventerous and fun. Everyday on our trips you pushed us to make the most of it. And that goes beyond our trips! Weekends, evenings, I'm so happy and thankful I have you here to make sure I don't let life pass me by.","https://i.imgur.com/fUn8UNz.jpg"],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""],
-    ["",""]
+    ["I'm sure your mom told you this numerous times growing up, but you're a rock. Always there when I need you, unwaivering in your faithfulness, and confident in your feelings and how you express them. I can always rely on you, I promise never to take that for granted.","https://media0.giphy.com/media/W5BWeQ3ZN0W0D0dM7e/200w.webp?cid=ecf05e477z773tfpp408f3zjwpnzn8ujelcxiyywl4y4wzw9&rid=200w.webp&ct=g", "https://media1.giphy.com/media/l44Q5hp1dwIoZcGsw/200w.webp?cid=ecf05e47baznifrftrtimc3hwr8agziqj7fzegiy0475w7k1&rid=200w.webp&ct=g"],
+    ["Any more patience and you would be a doctor! I've put you through so much, and I can not imagine the restraint it must have taken not to throw me off some of those mountain peaks! You not only refrain from killing me, but support me every single day.","https://media2.giphy.com/media/W68eZm2RllVAwIg84v/giphy.gif?cid=ecf05e479zelmmi5ml5z46emaybcw2y8zsw9gf2at4kizzxu&rid=giphy.gif&ct=g"],
+    ["It's wild to feel so understood. You and Rob are the only people to ever understand me at this level. Danny, Quinn, Steven, eh, mostly, but only with you do I feel that every word is taken exactly how it was meant to be recieved.","https://media3.giphy.com/media/ll0wq2CTo7LwqBJ7nX/200w.webp?cid=ecf05e47x5kg6jk5jg7256jhs4ndq5vpngznylo4xdlrodj3&rid=200w.webp&ct=g", "https://media0.giphy.com/media/JoV6WP96snO0DOnqEz/100.webp?cid=ecf05e474adqo1pvcu4wjds8t5vaudtti8k7mw8mmj3y1agu&rid=100.webp&ct=g"],
+    ["I could not imagine feeling any more loved or worthy of love. I used to not feel so confident! The way you look at me makes me feel like I could move the mountains we climb. Together I know we're unstoppable.","https://media1.giphy.com/media/Gswu2WjUX2KKHwK9cK/giphy.gif?cid=ecf05e47m2acphvzfhchlenhhzp0yh1fuvhox3dy7vxktlol&rid=giphy.gif&ct=g"],
+    ["There's nothing like a perfect hand-written letter from you to signify your love. Reading one of those, I know it's the real deal! They are so heart-felt and pure. They are a gift to which I will continue to look forward! (I need a binder & sleeves...) ","https://media3.giphy.com/media/KjvlWLTmKqAww/giphy.gif?cid=ecf05e47l322n3mmedr5gcdnmxqsgu9xm7qqijo36lzsvxwq&rid=giphy.gif&ct=g"],
+    ["Your other gifts are incredible as well. Even when just the present of your presense is enough, you are always go so above and beyond, finding items that far exceed what you get in return, yet you never stop amazing me with the effort you put in.","https://media1.giphy.com/media/l3vRebb6HyeIgvmQ8/giphy.gif?cid=ecf05e47zkcnzoqct57uify1qhba2l06crjwax6lpmui8po5&rid=giphy.gif&ct=g"],
+    ["You are just so dang smart! No matter how hard I've seen school and Kode push you, you've always stepped right up to the plate, learned what was necessary, and crushed it!","https://media0.giphy.com/media/NlOGOI5HB8BLUPrm8I/200w.webp?cid=ecf05e47cdq3u7gen4gtws8g5ao2h46cdf5l0zw3x7ac7usp&rid=200w.webp&ct=g"],
+    ["I know you're smart, because you're so dang funny. You get me laughing every night, even after discouraging days. Your sarcasm and mine are quickly converging, and I gotta say I like where they're going.","https://media4.giphy.com/media/ZqlvCTNHpqrio/200w.webp?cid=ecf05e47bfr1bgyt6d2vkoebl7f7zaf1eikwdy7o3d6ourwt&rid=200w.webp&ct=g"],
+    ["With the passing of Elizabeth I would like to crown you, my love, the Geography Queen! It's so impressive watching you complete those countries of the world quizzes. You'd think you would have a better sense of direction.","https://media3.giphy.com/media/9JgeSP0jlRAVBOG9FD/giphy.gif?cid=ecf05e47pq6r64qa5v78xdzqtrtjwhwe8jkpudenmpg913fh&rid=giphy.gif&ct=g"],
+    ["Speaking of online quizzes, when's the last time you took a typing test? Watching your fingers fly at 130WPM is mind-boggling. How do those little fingers dance to eloquently accross the keys? It's a hoot to listen to my keyboard click at such a brisk pace.","https://media3.giphy.com/media/ule4vhcY1xEKQ/200w.webp?cid=ecf05e47y8tbx4nrb6e0jm1l2n7z7r1cs7mjyires0wqqks9&rid=200w.webp&ct=g"],
+    ["Ok we go back and forth on this judgemental thing and whether it's okay... I think we've struck a perfect balance! Your judgements are, sound, just, and often have me dying of laughter. You never treat others unfairly, judge away!","https://media3.giphy.com/media/eTQHquc8rkiYl9oaUJ/200w.webp?cid=ecf05e47libxuyyv4zvb634wq5esui8t8168v1f1v5wzl647&rid=200w.webp&ct=g", "https://media1.giphy.com/media/oCodIpd8Tbo8TQvzlP/200w.webp?cid=ecf05e47libxuyyv4zvb634wq5esui8t8168v1f1v5wzl647&rid=200w.webp&ct=g"],
+    ["Get a bar on this girl's shoulders and she turns into the hulk! Put it on her hips and she'll thrust 200lbs airbourne like a feather! It was amazing watching your workout grind and progress, and I'm so excited for it to resume and to see where you end up.","https://i.imgur.com/rGlSKMd.gif"],
+    ["Ok last 5 are going to be me complementing your physicality (could have easily been all 25). Your hair, no matter what you do with it, is always so beautiful. Dark and silky smooth, but can somehow rock the perm! In terms of SFW locations for my hands, your hair is #1.","https://i.imgur.com/zDpG3ri.jpg"],
+    ["Her eyes, her eyes make the stars look like they're not shining! It's so sad, living in the city, not getting to see stars every night, sad for everyone else that is! I get lost in the twinkle of your eyes nightly. A beautiful window to a beautiful soul","https://i.imgur.com/aRZq5HS.jpg"],
+    ["What a contagious smile! This is my daily goal. Every day. Get this face to happen as much as possible. Not for your wellbeing of course, no no, just for me! I am the lucky one who gets to see those beautiful lips curl up to reveal this gorgeous grin.","https://i.imgur.com/7TZ5tMd.jpg"],
+    ["Your height! It's perfect. You are so many things because of it. 1 - a pain to bring to amusement parks. 2 - THE LIMBO CHAMPION. And 3, my favorite, the best and most comfortable cuddle buddy any 5ft7.5in person could ask for.", "https://i.imgur.com/nDko2Gl.jpg"],
+    ["Finally, my favorite thing about your face is without question your cute nose! Can't beat it. It's truly God's best work. Of course it has to sweat a little! It's in the rules that He can't make anything actually perfect. You're lucky I couldn't find a picture","https://media0.giphy.com/media/l3E6AaOpYEye7uWbK/giphy.gif?cid=ecf05e47oxks1zgksr19jkcg9uh4s8xtrg8498jo7flo3sxo&rid=giphy.gif&ct=g"],
+    ["Princess you deserve the best birthday ever. I hope we were able to catch a glimpse of the aurora borealis this weekend! Lucky me though, I do not need these lights, I get to see once in a lifetime beauty everytime I wake up in the morning.","https://i.imgur.com/wK7kIWX.jpg", "https://i.imgur.com/Y0OltgF.jpg"]
   ];
   
 const pagesElement = document.getElementById("pages");
@@ -36,7 +37,7 @@ pageTextAndPic.forEach(([content, ...srcs], ix) => {
     numPage.setAttribute('class', 'page')
     // the number
     let num = document.createElement('p')
-    num.innerText = `${ix + 1}.`
+    num.innerText = ix === 25 ? 'Closing remarks' : `${ix + 1}.`
     numPage.appendChild(num)
     // the image
     srcs.forEach((src) => {
@@ -60,8 +61,11 @@ pageTextAndPic.forEach(([content, ...srcs], ix) => {
 
 let blankLastPage = document.createElement('div')
 blankLastPage.setAttribute('class', 'page')
+let heart = document.createElement('p')
+heart.innerText = '❤️️'
 let text = document.createElement('p')
-text.innerText = '❤️️'
+text.innerText = 'To our never-ending adventure!'
+blankLastPage.appendChild(heart)
 blankLastPage.appendChild(text)
 pagesElement.appendChild(blankLastPage)
 
